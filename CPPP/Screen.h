62fda2ab798eb9f7b83dc38a0dfd0a222767ff48
周潇韵 <zgqyzxy@163.com>
2018-12-09 21:34:42 +0800

    :x: find a out-of-range bug of Screen.set()

--- CPPP/Screen.h
+++ CPPP/Screen.h
@@ -10,16 +10,16 @@ public:
     // constructor function
     Screen() = default;
     Screen(const pos &ht, const pos &wd, const char &c)
-        :height(ht), width(wd), contents(ht*wd, c) {}
-    Screen(const pos &ht, const pos& wd, const pos &num)
-        :height(ht), width(wd), contents(num, ' ') {}
-    Screen(const pos &ht,const pos &wd,const string & str)
-        :height(ht), width(wd), contents(str) {}
+        :height(ht), width(wd), contents(ht*wd, c) { }
+    Screen(const pos &ht, const pos& wd)
+        :height(ht), width(wd), contents(ht*wd, ' ') { }
     // normal memeber function
     char get() const {
         return contents[cursor];
     }
     inline char get(pos ht, pos wd) const;
+    Screen &set(char c);
+    Screen &set(pos r, pos col, char ch);
     Screen &move(pos r, pos c);
     void some_member() const;   // just a example function
 private:
@@ -28,6 +28,30 @@ private:
     std::string contents;
     mutable size_t access_ctr = 0;  // count times of calling some_member()
 };
+inline Screen & Screen::set(char c)
+{
+
+    try
+    {
+        if (contents.size() < cursor) {
+            throw std::out_of_range("The pos of cursor is out of range");
+        }
+        else {
+            contents[cursor] = c;
+        }
+    }
+    catch (const std::out_of_range& cursor_out_of_range)
+    {
+        cerr << cursor_out_of_range.what() << endl;
+        // 还需要补充如何解决越界问题
+    }
+    return *this;
+}
+inline Screen & Screen::set(pos r, pos col, char ch)
+{
+    contents[r*width + col] = ch;
+    return *this;
+}
 inline Screen &Screen::move(pos r, pos c) {
     pos row = r * width;
     cursor = row + c;
