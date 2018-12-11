9aa27499e6db8ce205ae2dd557a2ea2e22a2289f
周潇韵 <zgqyzxy@163.com>
2018-12-11 19:31:38 +0800

    :banana: add friend class Window_mgr

--- CPPP/Screen.h
+++ CPPP/Screen.h
@@ -2,6 +2,7 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 #include <string>
+//#include "Window_mgr.h"
 
 class Screen {
 public:
@@ -16,12 +17,14 @@ public:
     // normal memeber function
 	char get() const;
     char get(pos ht, pos wd) const;
-    Screen set(char c);
-    Screen set(pos r, pos col, char ch);
-    Screen move(pos r, pos c);
-	Screen display(std::ostream &os);
-	const Screen display(std::ostream &os) const;
+    Screen& set(char c);
+    Screen& set(pos r, pos col, char ch);
+    Screen& move(pos r, pos c);
+	Screen& display(std::ostream &os);
+	const Screen& display(std::ostream &os) const;
     void some_member() const;   // just a example function
+	// friend member
+	friend class Window_mgr;
 private:
     pos cursor = 0;
     pos height = 0, width = 0;
@@ -29,9 +32,8 @@ private:
     mutable size_t access_ctr = 0;  // count times of calling some_member()
 	void do_display(std::ostream &os) const;
 };
-inline Screen  Screen::set(char c)
+inline Screen& Screen::set(char c)
 {
-
     try
     {
         if (contents.size() < cursor) {
@@ -48,12 +50,12 @@ inline Screen  Screen::set(char c)
     }
     return *this;
 }
-inline Screen  Screen::set(pos r, pos col, char ch)
+inline Screen& Screen::set(pos r, pos col, char ch)
 {
     contents[r*width + col] = ch;
     return *this;
 }
-inline Screen Screen::move(pos r, pos c) {
+inline Screen& Screen::move(pos r, pos c) {
     pos row = r * width;
     cursor = row + c;
     return *this;
@@ -66,11 +68,11 @@ inline char Screen::get(pos r, pos c) const {
     pos row = r * width;
     return contents[row + c];
 }
-inline Screen Screen::display(std::ostream &os) {
+inline Screen& Screen::display(std::ostream &os) {
 	do_display(os);
 	return *this;
 }
-inline const Screen Screen::display(std::ostream &os) const {
+inline const Screen& Screen::display(std::ostream &os) const {
 	do_display(os);
 	return *this;
 }
