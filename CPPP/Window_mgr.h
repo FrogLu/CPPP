407282bfdfea7795e8fca1eb8fd29bca85e836c3
周潇韵 <zgqyzxy@163.com>
2018-12-11 19:32:07 +0800

    Revert ":banana: add friend class Window_mgr"
    This reverts commit 9aa27499e6db8ce205ae2dd557a2ea2e22a2289f.

--- CPPP/Window_mgr.h
+++ CPPP/Window_mgr.h
@@ -5,15 +5,8 @@
 #include <vector>
 
 class Window_mgr {
-public:
-	using ScreenIndex = std::vector<Screen>::size_type;
-	void clear(ScreenIndex);
 private:
     std::vector<Screen> screens{ Screen(24, 80, ' ') };
 };
-inline void Window_mgr::clear(ScreenIndex index) {
-	Screen &s = screens[index];
-	s.contents = string(s.height*s.width, ' ');
-}
 #endif // !Window_MGR_H
 
