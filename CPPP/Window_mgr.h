28f46395df57ee341e4d6427f96e323e82ba8016
周潇韵 <zgqyzxy@163.com>
2018-12-12 14:59:05 +0800

    :banana: update Chapter 7.4.0

--- CPPP/Window_mgr.h
+++ CPPP/Window_mgr.h
@@ -8,12 +8,18 @@ class Window_mgr {
 public:
 	using ScreenIndex = std::vector<Screen>::size_type;
 	void clear(ScreenIndex);
+	ScreenIndex addScreen(const Screen& screen);
 private:
     std::vector<Screen> screens{ Screen(24, 80, ' ') };
 };
 inline void Window_mgr::clear(ScreenIndex index) {
 	Screen &s = screens[index];
 	s.contents = string(s.height*s.width, ' ');
+}
+inline Window_mgr::ScreenIndex Window_mgr::addScreen(const Screen & screen)
+{
+	screens.push_back(screen);
+	return screens.size() - 1;
 }
 #endif // !Window_MGR_H
 
