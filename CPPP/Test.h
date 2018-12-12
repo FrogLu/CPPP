f07e8ca2b194b9616161d68f922827505631a09d
周潇韵 <zgqyzxy@163.com>
2018-12-12 15:47:34 +0800

    :banana: Practice 7.35

--- CPPP/Test.h
+++ CPPP/Test.h
@@ -8,4 +8,18 @@ class X {
 class Y {
 	X Y;
 };
+typedef string Type;
+Type initVal();
+class Exercise {
+public:
+	typedef double Type;
+	Type setVal(Type);
+	Type initVal() { return 1.0; };
+private:
+	int val;
+};
+Exercise::Type Exercise::setVal(Type parm) {
+	val = parm + initVal();
+	return val;
+}
 #endif
