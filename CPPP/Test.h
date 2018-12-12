9fb80b576806d3ab454d79a2ce4da5fa8ae2a153
周潇韵 <zgqyzxy@163.com>
2018-12-12 16:03:26 +0800

    :beer: add tips for Practice 7.31

--- CPPP/Test.h
+++ CPPP/Test.h
@@ -1,11 +1,14 @@
 #pragma once
 #ifndef _TEST_H_
 #define _TEST_H_
-class Y;
-class X {
-	Y * X;
-};
+/****这是为了说明：可以定义一个指针指向一个不完全类型的类(仅有声明)*****/
+/*但不能在类的定义中使用一个一个不完全类型的类，也就是说要先指针，后类型*/
+class X;
 class Y {
-	X Y;
+	X* ymem;
 };
+class X {
+	Y xmem;
+};
+
 #endif
