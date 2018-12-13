1401482dd825247e7cecfac621367d2985abb358
周潇韵 <zgqyzxy@163.com>
2018-12-13 14:04:55 +0800

    :banana: Chapter 7.5.3

--- CPPP/Test.h
+++ CPPP/Test.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef _TEST_H_
 #define _TEST_H_
+#include <string>
 /****这是为了说明：可以定义一个指针指向一个不完全类型的类(仅有声明)*****/
 /*但不能在类的定义中使用一个一个不完全类型的类，也就是说要先指针，后类型*/
 class X;
@@ -11,4 +12,17 @@ class X {
 	Y xmem;
 };
 
+class NoDefault {
+public:
+
+	NoDefault(const std::string& = " ") {};
+};
+struct A {
+	NoDefault my_mem;
+}; 
+A a;
+struct B {
+	//B() {}
+	NoDefault b_member;
+};
 #endif
