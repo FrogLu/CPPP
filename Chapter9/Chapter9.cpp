ad114b7365fe78c8365dbfc75001c4f228b8f008
周潇韵 <zgqyzxy@163.com>
2018-12-23 20:38:51 +0800

    :memo: Practice9.22

--- Chapter9/Chapter9.cpp
+++ Chapter9/Chapter9.cpp
@@ -1,34 +1,34 @@
 #include "pch.h"
 #include <iostream>
 using namespace std;
+#define some_val 3
 int main()
 {
 	std::ifstream fin("input2.txt");
 	fin.tie(&cout);
-	list<int> slist;
+	vector<int> ivec;
 	deque<int> ideq1, ideq2;
 	int val;
 	while (fin >> val) {
-		slist.push_front(val);
+		ivec.push_back(val);
 	}
-	for (auto iter = slist.begin(); iter !=slist.end(); ++iter)
-	{
-		if (*iter % 2 == 0) {
-			ideq1.push_back(*iter);
+	cout << "The raw data is ";
+	for (auto &val : ivec) {
+		cout << val << " ";
+	}
+	cout << endl;
+	std::vector<int>::iterator iter = ivec.begin(),
+		mid = ivec.begin() + ivec.size() / 2;
+	while (iter != (ivec.begin()+ivec.size()/2)) {
+		if (*iter == some_val) {
+			iter=ivec.insert(iter, 2 * some_val);
+			++iter;
+			++iter;
+			cout << *iter << endl;
 		}
 		else {
-			ideq2.push_back(*iter);
+			++iter;
 		}
 	}
-	cout << "The even numbers are: ";
-	for (auto &val : ideq1) {
-		cout << val << " ";
-	}
-	cout << endl;
-	cout << "The odd numbers are: ";
-	for (auto &val : ideq2) {
-		cout << val << " ";
-	}
-	cout << endl;
 	return 0;
 }
