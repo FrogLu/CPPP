22002a4161c1532c3c203dede4735c9d8bdb6989
周潇韵 <zgqyzxy@163.com>
2018-12-23 20:03:07 +0800

    :memo: Practice9.20

--- Chapter9/Chapter9.cpp
+++ Chapter9/Chapter9.cpp
@@ -3,16 +3,31 @@
 using namespace std;
 int main()
 {
-	std::ifstream fin("input1.txt");
+	std::ifstream fin("input2.txt");
 	fin.tie(&cout);
-	list<string> slist;
-	string str;
-	while (fin >> str) {
-		slist.push_front(str);
+	list<int> slist;
+	deque<int> ideq1, ideq2;
+	int val;
+	while (fin >> val) {
+		slist.push_front(val);
 	}
 	for (auto iter = slist.begin(); iter !=slist.end(); ++iter)
 	{
-		cout << *iter << " ";
+		if (*iter % 2 == 0) {
+			ideq1.push_back(*iter);
+		}
+		else {
+			ideq2.push_back(*iter);
+		}
+	}
+	cout << "The even numbers are: ";
+	for (auto &val : ideq1) {
+		cout << val << " ";
+	}
+	cout << endl;
+	cout << "The odd numbers are: ";
+	for (auto &val : ideq2) {
+		cout << val << " ";
 	}
 	cout << endl;
 	return 0;
