0a30ac498fcd74122aea28289a9664f986e2c08e
周潇韵 <zgqyzxy@163.com>
2018-12-23 19:44:17 +0800

    :memo: Chapter9.3.1

--- Chapter9/Chapter9.cpp
+++ Chapter9/Chapter9.cpp
@@ -3,25 +3,14 @@
 using namespace std;
 int main()
 {
-	std::ifstream fin1("input1.txt");
-	std::ifstream fin2("input2.txt");
-	fin1.tie(&cout);
-	fin2.tie(&cout);
-	vector<int> ivec1;
-	vector<int> ivec2;
-	int val1, val2;
-	while (fin1 >> val1) {
-		ivec1.push_back(val1);
-	}
-	while (fin2 >> val2) {
-		ivec2.push_back(val2);
-	}
-	if (ivec1 == ivec2) {
-		cout << std::boolalpha << true << endl;
-	}
-	else {
-		cout << std::boolalpha << false << endl;
-	}
+	vector<string> svec = { "quasi","simba","frollo","scar" };
+	list<string> slist;
+
+	cout << *slist.insert(slist.begin(), { "xixi" ,"Hello!" ,"world!"}) 
+		<< endl;
+	cout << *slist.insert(slist.end(), svec.begin(), svec.end()) << endl;
+	cout << *slist.insert(slist.end(), { "xixi" ,"Hello!" ,"world!" }) 
+		<< endl;
 
 	return 0;
 }
