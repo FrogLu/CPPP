dbb29650f236b117fc4d9f241bba3f58eade4592
周潇韵 <zgqyzxy@163.com>
2018-12-23 21:59:52 +0800

    :memo: Practice9.26

--- Chapter9/Chapter9.cpp
+++ Chapter9/Chapter9.cpp
@@ -7,17 +7,48 @@ int main()
 	std::ifstream fin("input2.txt");
 	fin.tie(&cout);
 	vector<int> ivec;
-	deque<int> ideq1, ideq2;
+	list<int> ilist;
 	int val;
 	while (fin >> val) {
 		ivec.push_back(val);
 	}
-	// 如果在空容器下运行会抛出out_of_range异常
-	if (!ivec.empty()) {
-		cout << "The first member(at): " << ivec.at(0) << endl;
-		cout << "The first member(subscript): " << ivec[0] << endl;
-		cout << "The first member(front()): " << ivec.front() << endl;
-		cout << "The first member(begin()): " << *ivec.begin() << endl;
+	ilist.assign(ivec.begin(),ivec.end());
+	auto liter = ilist.begin();
+	cout << "list: ";
+	for (auto liter = ilist.begin(); liter != ilist.end(); ++liter)
+	{
+		cout << *liter << " ";
 	}
+	cout << endl;
+	cout << "vector: ";
+	for (auto iter = ivec.begin(); iter != ivec.end(); ++iter)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+	liter = ilist.begin();
+	for (auto iter = ivec.begin(); iter != ivec.end(); )
+	{
+		if (*iter & 1) {
+				liter = ilist.erase(liter);
+				++iter;
+		}
+		else {
+				iter = ivec.erase(iter);
+				++liter;
+		}
+	}
+	cout << "list: ";
+	for (auto liter = ilist.begin(); liter != ilist.end(); ++liter)
+	{
+		cout << *liter << " ";
+	}
+	cout << endl;
+	cout << "vector: ";
+	for (auto iter = ivec.begin(); iter != ivec.end(); ++iter)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
 	return 0;
 }
