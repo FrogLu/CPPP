825df073b3299855e07ec98c5bbdb87ce24db707
周潇韵 <zgqyzxy@163.com>
2018-12-29 09:38:21 +0800

    :memo: Practice10.2

--- Chapter9/Chapter9.cpp
+++ Chapter9/Chapter9.cpp
@@ -8,11 +8,11 @@ int main()
 	fin.tie(&cout);
 	int val;
 	const int target = 4519;
-	vector<int> ivec;
+	list<int> ilst;
 	while (fin >> val) {
-		ivec.push_back(val);
+		ilst.push_back(val);
 	}
 	cout << "The count of " << target << " : ";
-	cout << count(ivec.begin(), ivec.end(), target) << endl;
+	cout << count(ilst.begin(), ilst.end(), target) << endl;
 	return 0;
 }
