5019253f34d718fd50dabe581695400f1cb60af1
周潇韵 <zgqyzxy@163.com>
2019-01-11 22:24:35 +0800

    :memo: Practice10.30

--- Chapter10/Chapter10.cpp
+++ Chapter10/Chapter10.cpp
@@ -5,15 +5,14 @@
 int main()
 {
 	/* fin starts */
-	std::ifstream fin("./data/InputStr");
+	std::ifstream fin("./data/InputInt");
 	assert(fin.good());
 	fin.tie(&cout);
-	std::istream_iterator<string> isiter(fin), eof;
-	std::vector<std::string> svec(isiter, eof);
-	for (auto c : svec) {
-		std::cout << c << " ";
-	}
-	std::cout << std::endl;
+	std::istream_iterator<int> isiter(fin), eof;
+	std::ostream_iterator<int> ositer(cout," ");
+	std::vector<int> ivec(isiter, eof);
+	std::sort(ivec.begin(), ivec.end());
+	std::copy(ivec.begin(), ivec.end(), ositer);
 
 	return 0;
 }
