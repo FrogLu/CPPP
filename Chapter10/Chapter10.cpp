78a48b940234bf598b907733f27db169510c20a2
周潇韵 <zgqyzxy@163.com>
2019-01-12 19:23:19 +0800

    :memo: Practice10.37

--- Chapter10/Chapter10.cpp
+++ Chapter10/Chapter10.cpp
@@ -6,27 +6,22 @@ int main()
 {
 	/* fin starts */
 	std::ifstream fin("./data/InputInt");
-	std::ofstream fout1("./data/Output1", std::ostream::app);
-	std::ofstream fout2("./data/Output2", std::ostream::app);
 	assert(fin.good());
-	assert(fout1.good());
-	assert(fout2.good());
 	fin.tie(&cout);
 	std::istream_iterator<int> isiter(fin), eof;
-	std::list<int> ilst(isiter, eof);
-	for (auto val : ilst) {
-		std::cout << val << " ";
+	std::ostream_iterator<int> ositer(cout, " ");
+	std::list<int> ilst;
+	std::vector<int> ivec(isiter,eof);
+	std::cout << "vector member: " << std::endl;
+	for (auto val : ivec) {
+		ositer = val;
 	}
 	std::cout << std::endl;
-	const int number = 9;
-	auto last_z = find(ilst.crbegin(), ilst.crend(), number);
-	int pos = 0;
-	for (auto iter = ilst.cbegin(); iter != last_z.base(); ++iter, ++pos);
-	if (!pos) {
-		std::cout << "Can't find number " << number << " in container" << std::endl;
-	}
-	else {
-		std::cout << "last pos of number " << number << " is: " << pos << std::endl;
+	ilst.assign(ivec.rend() - 7, ivec.rend() - 2);	// 顺序容器的左闭右开区间假定
+													// 所以后者是-2不是-3.
+	std::cout << "list member: " << std::endl;
+	for (auto val : ilst) {
+		ositer=val;
 	}
 
 	return 0;
