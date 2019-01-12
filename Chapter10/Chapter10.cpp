8a63f828c3b7d6d6553b6993c8f7ce8b1a21046a
周潇韵 <zgqyzxy@163.com>
2019-01-12 16:15:49 +0800

    :memo: Practice10.33

--- Chapter10/Chapter10.cpp
+++ Chapter10/Chapter10.cpp
@@ -5,29 +5,18 @@
 int main()
 {
 	/* fin starts */
-	std::ifstream fin("./data/Sales_data");
+	std::ifstream fin("./data/InputInt");
+	std::ofstream fout1("./data/Output1", std::ostream::app);
+	std::ofstream fout2("./data/Output2", std::ostream::app);
 	assert(fin.good());
+	assert(fout1.good());
+	assert(fout2.good());
 	fin.tie(&cout);
-	std::istream_iterator<Sales_data> isiter(fin), eof;
-	std::ostream_iterator<Sales_data> ositer(cout,"\n");
-	std::vector<Sales_data> salevec(isiter, eof);
-	if (salevec.empty()) {
-		std::cerr << "No data!" << std::endl;
-		return -1;
-	}
-	std::sort(salevec.begin(), salevec.end(),compareIsbn);
-	for (auto sale : salevec) {
-		*ositer++ = sale;
-	}
-	std::cout << "The sum: " << std::endl;
-	auto iter = salevec.begin();
-	while (iter != salevec.end()) {
-		auto currdata = *iter;
-		auto pos = find_if(iter + 1, salevec.end(),
-			[currdata](const Sales_data &data1)
-			{return data1.isbn() != currdata.isbn(); });
-		std::cout << accumulate(iter + 1, pos, currdata) << std::endl;
-		iter = pos;
+	std::istream_iterator<int> isiter(fin), eof;
+	std::ostream_iterator<int> ositer1(fout1, " "), ositer2(fout2, "\n");
+	while (isiter != eof) {
+		(*isiter) % 2 ? ositer1 = *isiter : ositer2 = *isiter;
+		++isiter;
 	}
 
 	return 0;
