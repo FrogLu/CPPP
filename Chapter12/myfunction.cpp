2a5641ac48c674a48ad535ead0c25199d6c3064b
周潇韵 <zgqyzxy@163.com>
2019-02-01 11:35:14 +0800

    :memo: Practice12.15

--- Chapter12/myfunction.cpp
+++ Chapter12/myfunction.cpp
@@ -3,12 +3,11 @@
 
 connection connect(destination*) { connection c; return c; };
 void disconnect(connection) {};
-
 void end_connection(connection* p) {
     disconnect(*p);
 }
 
 void f(destination& d) {
     connection c = connect(&d);
-    std::shared_ptr<connection> p(&c, end_connection);
+    std::shared_ptr<connection> p(&c, [](connection * p) {disconnect(*p); });
 }
