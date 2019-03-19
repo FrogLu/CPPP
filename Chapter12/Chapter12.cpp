7a26f0b32094f1f7508c3db1931ac93fa7128861
FrogLu <zgqyzxy@163.com>
2019-03-19 20:03:31 +0800

    :memo: Practice12.23 version1

--- Chapter12/Chapter12.cpp
+++ Chapter12/Chapter12.cpp
@@ -4,9 +4,17 @@
 
 int main()
 {
-    StrBlob cSB = { "hello","world" };
-    for (auto it = cSB.begin(); !eq(it, cSB.end()); it.incr()) {
-        std::cout << it.deref() << std::endl;
+    std::string  str1, str2;
+    std::cin >> str1 >> str2;
+    std::string str(str1 + str2);
+    char* p = new char[str.length()]();
+    for (auto iter = str.cbegin(); iter != str.cend();++iter) {
+        std::cout << "befor: " << *p << std::endl;
+        (*p) = (*iter);
+        std::cout << "after: " << *p << std::endl;
+        ++p;
     }
+    delete[] p;
+
     return 0;
 }
