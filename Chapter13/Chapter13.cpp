4d21a04d6de0709429765abe60b67aa6eaeb71b4
FrogLu <zgqyzxy@163.com>
2019-03-28 20:47:00 +0800

    :memo: Practice 13.13 RefAns

--- Chapter13/Chapter13.cpp
+++ Chapter13/Chapter13.cpp
@@ -1,13 +1,47 @@
 #include "pch.h"
 #include "HasPtr.h"
 
+void f1(HasPtr hp) {
+
+}
+
+void f2(HasPtr& hp) {
+
+}
+
 int main()
 {
-    std::string str("hello world");
-    {
-        HasPtr p1(str);
-        HasPtr p2 = p1;
-    }
+    std::cout << "Local variable: " << std::endl;
+    HasPtr hp;
+    std::cout << std::endl;
+
+    std::cout << "Non-reference parameter: " << std::endl;
+    f1(hp);
+    std::cout << std::endl;
+
+    std::cout << "Reference parameter: " << std::endl;
+    f2(hp);
+    std::cout << std::endl;
+
+    std::cout << "Dynamic allocation: " << std::endl;
+    HasPtr* php = new HasPtr;
+    std::cout << std::endl;
+
+    std::cout << "Add in container: " << std::endl;
+    std::vector<HasPtr> vhp;
+    vhp.push_back(hp);
+    std::cout << std::endl;
+
+    std::cout << "Release dynamic allocation object: " << std::endl;
+    delete php;
+    std::cout << std::endl;
+
+    std::cout << "Indirect initialization and assignment: " << std::endl;
+    HasPtr y = hp;
+    y = hp;
+    std::cout << std::endl;
+
+    std::cout << "End of program: " << std::endl;
 
     return 0;
 }
--- Chapter13/pch.h
+++ Chapter13/pch.h
@@ -2,5 +2,7 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <vector>
 
 using std::string; using std::shared_ptr; using std::make_shared;
+using std::vector;
