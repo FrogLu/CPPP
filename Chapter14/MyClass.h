6bba4335a77d211b453a739ead36945bf907697e
FrogLu <zgqyzxy@163.com>
2019-04-15 21:15:59 +0800

    :memo: Practice14.34 from RefAns

--- Chapter14/MyClass.h
+++ Chapter14/MyClass.h
@@ -4,17 +4,16 @@
 
 class MyIfElse {
 public:
-    std::size_t operator()(const bool condition, const std::size_t& first, const std::size_t& last);
+    MyIfElse() = default;
+    MyIfElse(const std::size_t i1, const std::size_t i2, const std::size_t i3) :iVal1(i1),iVal2(i2), iVal3(i3) {}
+    std::size_t operator()(const std::size_t, const std::size_t& first, const std::size_t& last);
+private:
+    const std::size_t iVal1, iVal2, iVal3;
 };
 
 inline
-std::size_t MyIfElse::operator()(const bool condition, const std::size_t& first, const std::size_t& last) {
-    if (condition) {
-        return first;
-    }
-    else {
-        return last;
-    }
+std::size_t MyIfElse::operator()(const std::size_t condition, const std::size_t& first, const std::size_t& last) {
+    return condition ? first : last;
 }
 
 #endif // !_MYCLASS_H_
