44519c7150011260b35cfc4ebf9aaca492184f02
FrogLu <zgqyzxy@163.com>
2019-04-15 21:35:52 +0800

    :memo: Practice14.36

--- Chapter14/Chapter14.cpp
+++ Chapter14/Chapter14.cpp
@@ -3,14 +3,13 @@
 #include "Sales_data.h"
 #include "StrBlob.h"
 #include "StrVec.h"
+#include "MyClass.h"
 
 int main() {
-    StrVec svec({ "hello"});
-    String str("world");
-    StrBlob sb({ "FrogLu" });
-    std::cout << "svec[0]: " << svec[0] << std::endl;
-    std::cout << "str[0]: " << str[0] << std::endl;
-    std::cout << "sb[0]: " << sb[0] << std::endl;
+    std::vector<std::string> svec;
+    ReadString str;
+    PrintString printStr(std::cout);
+    printStr(str(std::cin));
 
     return 0;
 }
