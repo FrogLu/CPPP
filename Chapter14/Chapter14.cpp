a2758f71c717b3e92c6a710b4e35f5972ca85cb4
FrogLu <zgqyzxy@163.com>
2019-04-16 18:52:54 +0800

    :memo: Practice14.43

--- Chapter14/Chapter14.cpp
+++ Chapter14/Chapter14.cpp
@@ -8,22 +8,29 @@
 
 int main() {
     std::ifstream fint("./data/Input/int");
-    std::ifstream fstr("./data/Input/paper");
     assert(fint.good());
-    assert(fstr.good());
     fint.tie(&std::cout);
-    fstr.tie(&std::cout);
     std::istream_iterator<std::size_t> IsStIter(fint), StEeof;
-    std::istream_iterator<std::string> IsStrIter(fstr), StrEof;
     std::vector<std::size_t> stvec(IsStIter, StEeof);
-    std::vector<std::string> svec(IsStrIter, StrEof);
-    //  Practice14.42
-    auto a = std::count_if(stvec.cbegin(), stvec.cend(), 
-        std::bind(std::greater<std::size_t>(), std::placeholders::_1,1024));
-    auto b = std::find_if(svec.cbegin(), svec.cend(),
-        std::bind(std::not_equal_to<std::string>(), std::placeholders::_1, std::string("pooh")));
-    auto c = std::transform(stvec.begin(), stvec.end(), stvec.begin(),
-        std::bind(std::multiplies<std::size_t>(), std::placeholders::_1, 2));
+    // Practice14.43
+    while (true) {
+        std::cout << "Please Enter the modulated number: ";
+        std::size_t mod;
+        std::cin >> mod;
+        if (std::find_if(stvec.cbegin(), stvec.cend(),
+            std::bind(std::modulus<std::size_t>(), std::placeholders::_1, mod)) == stvec.cend()) {
+            std::cout << "Number: " << mod << "works" << std::endl;
+            break;
+        }
+        std::cout << "Press q to quit, others to continue: ";
+        char command = ' ';
+        assert(std::cin >> command);
+        if (command == 'q') {
+            break;
+        }
+    }
+    
+    
 
     return 0;
 }
