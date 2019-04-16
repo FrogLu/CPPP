c0920f0d0ae6efb4e6bdb904fc2ed2639aaf1bc5
FrogLu <zgqyzxy@163.com>
2019-04-16 13:12:56 +0800

    :memo: Practice14.39

--- Chapter14/Chapter14.cpp
+++ Chapter14/Chapter14.cpp
@@ -16,12 +16,16 @@ int main() {
         std::cout << s << " ";
     }
     std::cout << std::endl;
-    for (std::string::size_type threshold = 1; threshold != 11; ++threshold) {
+    std::size_t count = 0;
+    const std::size_t begin = 1, end = 10;
+    for (std::string::size_type threshold = begin; threshold != end; ++threshold) {
         CheckSize cs(threshold);
-        auto count = std::count_if(svec.cbegin(), svec.cend(), cs);
-        std::cout << "count of word's length " << threshold << " in paper: " << count << std::endl;
+        count += std::count_if(svec.cbegin(), svec.cend(), cs);
         countVec.push_back(count);
     }
+    std::cout << "count of word's length " << begin << "-" << end - 1 << " in paper: ";
+    std::cout << count << std::endl;
+    std::cout << "length more than: " << end << " in paper: " << svec.size() - count << std::endl;
     
 
     return 0;
