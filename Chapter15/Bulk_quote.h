6bd848f32dc5f04b97f9ad391e67f25d5eed7d2f
FrogLu <zgqyzxy@163.com>
2019-04-18 21:39:04 +0800

    :memo: Practice15.16

--- Chapter15/Bulk_quote.h
+++ Chapter15/Bulk_quote.h
@@ -9,7 +9,7 @@ class Bulk_quote :
 public:
     Bulk_quote() = default;
     Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) :
-        Disc_quote(book, p,qty,disc) {}
+        Disc_quote(book, p, qty, disc) {}
     virtual double net_price(std::size_t) const override;
     virtual std::ostream& debug(std::ostream& os) const override;
     virtual ~Bulk_quote() = default;
--- Chapter15/Limited_quote.h
+++ Chapter15/Limited_quote.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "Quote.h"
+#include "Disc_quote.h"
 class Limited_quote :
-    public Quote
+    public Disc_quote
 {
 public:
     Limited_quote() = default;
     Limited_quote(const std::string& bookNo, double p, std::size_t qty, double disc) :
-        Quote(bookNo, p), max_qty(qty), discount(disc) {}
+        Disc_quote(bookNo, p, qty, disc) {}
     virtual double net_price(std::size_t cnt) const override;
     virtual std::ostream& debug(std::ostream& os) const override;
     virtual ~Limited_quote() = default;
