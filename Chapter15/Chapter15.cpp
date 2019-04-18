406a8c50471e8f00badaa0c364a2225c29a85280
FrogLu <zgqyzxy@163.com>
2019-04-18 21:44:26 +0800

    :memo: Practice15.17

--- Chapter15/Chapter15.cpp
+++ Chapter15/Chapter15.cpp
@@ -2,12 +2,16 @@
 #include "Quote.h"
 #include "Bulk_quote.h"
 #include "Limited_quote.h"
+#include "Disc_quote.h"
 
 int main() {
     Quote qt("FrogLu", 11.5);
     Bulk_quote blqt("xixi", 11.5, 5, 0.2);
     Limited_quote lmqt("CPPP", 11.5, 3, 0.2);
     Quote *demo = &blqt;
+    //  Error C2259 'Disc_quote': cannot instantiate abstract class
+    //  Error(active) E0322	object of abstract class type "Disc_quote" is not allowed
+    Disc_quote dsqt;    //  Error
     qt.debug(std::cout);
     std::cout << std::endl;
     blqt.debug(std::cout);
