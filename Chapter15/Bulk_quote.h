44da35dd816781916d926532d3e9ef7cef872e69
FrogLu <zgqyzxy@163.com>
2019-04-18 11:51:33 +0800

    :memo: Practice15.5

---
+++ Chapter15/Bulk_quote.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _BULK_QUOTE_H_
+#define _BULK_QUOTE_H_
+
+#include "Quote.h"
+class Bulk_quote :
+    public Quote
+{
+public:
+    Bulk_quote() = default;
+    Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) :
+        Quote(book, p), min_qty(qty), discount(disc) {}
+    double net_price(std::size_t) const override;
+    virtual ~Bulk_quote() = default;
+private:
+    std::size_t min_qty = 0;
+    double discount = 0.0;
+};
+
+#endif // !_BULK_QUOTE_H
