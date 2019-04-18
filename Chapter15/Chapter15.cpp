e519615a4f5d61be287f27fe761cc175b4c980bd
FrogLu <zgqyzxy@163.com>
2019-04-18 20:04:28 +0800

    :memo: test for Practice15.14(f)

--- Chapter15/Chapter15.cpp
+++ Chapter15/Chapter15.cpp
@@ -7,12 +7,15 @@ int main() {
     Quote qt("FrogLu", 11.5);
     Bulk_quote blqt("xixi", 11.5, 5, 0.2);
     Limited_quote lmqt("CPPP", 11.5, 3, 0.2);
+    Quote *demo = &blqt;
     qt.debug(std::cout);
     std::cout << std::endl;
     blqt.debug(std::cout);
     std::cout << std::endl;
     lmqt.debug(std::cout);
     std::cout << std::endl;
+    std::cout << std::endl;
+    demo->debug(std::cout);
 
     return 0;
 }
