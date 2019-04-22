2c171619d4c48ffe1a130b64073715e1f4d929c7
FrogLu <zgqyzxy@163.com>
2019-04-22 21:08:49 +0800

    :memo: Practice15.27

--- Chapter15/Bulk_quote.h
+++ Chapter15/Bulk_quote.h
@@ -7,13 +7,14 @@ class Bulk_quote :
     public Disc_quote
 {
 public:
-    Bulk_quote() = default;
-    Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) :
-        Disc_quote(book, p, qty, disc) {}
-    Bulk_quote(const Bulk_quote&);
-    Bulk_quote(Bulk_quote&&)noexcept;
-    Bulk_quote& operator=(const Bulk_quote&);
-    Bulk_quote& operator=(Bulk_quote&&)noexcept;
+    //Bulk_quote() = default;
+    //Bulk_quote(const std::string& book, double p, std::size_t qty, double disc) :
+    //    Disc_quote(book, p, qty, disc) {}
+    //Bulk_quote(const Bulk_quote&);
+    //Bulk_quote(Bulk_quote&&)noexcept;
+    //Bulk_quote& operator=(const Bulk_quote&);
+    //Bulk_quote& operator=(Bulk_quote&&)noexcept;
+    using Disc_quote::Disc_quote;
     virtual double net_price(std::size_t) const override;
     virtual std::ostream& debug(std::ostream& os) const override;
     virtual ~Bulk_quote();
@@ -22,50 +23,50 @@ private:
     double discount = 0.0;
 };
 
-inline
-Bulk_quote::Bulk_quote(const Bulk_quote& blqt):
-    Disc_quote(blqt)
-{
-    min_qty = blqt.min_qty;
-    discount = blqt.discount;
-
-    std::cout << "Bulk_quote::Bulk_quote(const Bulk_quote& blqt)" << std::endl;
-}
-
-inline 
-Bulk_quote::Bulk_quote(Bulk_quote&& blqt) noexcept:
-    Disc_quote(std::move(blqt))
-{
-    min_qty = std::move(blqt.min_qty);
-    discount = std::move(blqt.discount);
-
-    std::cout << "Bulk_quote::Bulk_quote(Bulk_quote&& blqt)" << std::endl;
-}
-
-inline 
-Bulk_quote& Bulk_quote::operator=(const Bulk_quote& rhs)
-{
-    Disc_quote::operator=(rhs);
-
-    min_qty = rhs.min_qty;
-    discount = rhs.discount;
-
-    std::cout << "Bulk_quote::operator=(const Bulk_quote& rhs)" << std::endl;
-
-    return *this;
-}
-
-inline 
-Bulk_quote& Bulk_quote::operator=(Bulk_quote&& blqt) noexcept
-{
-    Disc_quote::operator=(blqt);
-
-    min_qty = std::move(blqt.min_qty);
-    discount = std::move(blqt.discount);
-
-    std::cout << "Bulk_quote::operator=(Bulk_quote&& blqt)" << std::endl;
-
-    return *this;
-}
+//inline
+//Bulk_quote::Bulk_quote(const Bulk_quote& blqt):
+//    Disc_quote(blqt)
+//{
+//    min_qty = blqt.min_qty;
+//    discount = blqt.discount;
+//
+//    std::cout << "Bulk_quote::Bulk_quote(const Bulk_quote& blqt)" << std::endl;
+//}
+//
+//inline 
+//Bulk_quote::Bulk_quote(Bulk_quote&& blqt) noexcept:
+//    Disc_quote(std::move(blqt))
+//{
+//    min_qty = std::move(blqt.min_qty);
+//    discount = std::move(blqt.discount);
+//
+//    std::cout << "Bulk_quote::Bulk_quote(Bulk_quote&& blqt)" << std::endl;
+//}
+//
+//inline 
+//Bulk_quote& Bulk_quote::operator=(const Bulk_quote& rhs)
+//{
+//    Disc_quote::operator=(rhs);
+//
+//    min_qty = rhs.min_qty;
+//    discount = rhs.discount;
+//
+//    std::cout << "Bulk_quote::operator=(const Bulk_quote& rhs)" << std::endl;
+//
+//    return *this;
+//}
+//
+//inline 
+//Bulk_quote& Bulk_quote::operator=(Bulk_quote&& blqt) noexcept
+//{
+//    Disc_quote::operator=(blqt);
+//
+//    min_qty = std::move(blqt.min_qty);
+//    discount = std::move(blqt.discount);
+//
+//    std::cout << "Bulk_quote::operator=(Bulk_quote&& blqt)" << std::endl;
+//
+//    return *this;
+//}
 
 #endif // !_BULK_QUOTE_H
--- Chapter15/Chapter15.cpp
+++ Chapter15/Chapter15.cpp
@@ -6,11 +6,7 @@
 #include "Base.h"
 
 int main() {
-    Quote qt1, qt2(qt1),qt3;
-    qt3 = qt2;
-    //Disc_quote dsqt1, dsqt2(dsqt1), dsqt3;  //  E0322   object of abstract class type "Disc_quote" is not allowed 
-    //dsqt3 = dsqt2;
-    Bulk_quote blqt1, blqt2(blqt1), blqt3;
+    Bulk_quote blqt1("FrogLu",12.5,6,0.2), blqt2(blqt1), blqt3;
     blqt3 = blqt2;
 
     return 0;
