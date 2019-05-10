b973363c2384f827f7f4a82c3706f9ae109e6293
FrogLu <zgqyzxy@163.com>
2019-05-10 08:43:31 +0800

    :memo: Practice16.3

--- Chapter16/Chapter16.cpp
+++ Chapter16/Chapter16.cpp
@@ -1,11 +1,11 @@
 #include "pch.h"
 #include "compare.h"
-
+#include "../Chapter14/Sales_data.h"
 
 int main()
 {
-    int a = -1, b = 1;
-    std::cout << compare(a, b) << std::endl;
+    Sales_data sd1, sd2;
+    std::cout << compare(sd1, sd2) << std::endl;
 
     return 0;
 }
